ce89aa2db2831e164b2a91269bba819a1e322f9a
PhoenixARC <46140834+PhoenixARC@users.noreply.github.com>
2020-10-31 14:30:33 -0400

    1.1

--- FUI Label Finder/Properties/Resources.Designer.cs
+++ FUI Label Finder/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace FUI_Label_Finder.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap image0 {
-            get {
-                object obj = ResourceManager.GetObject("image0", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
